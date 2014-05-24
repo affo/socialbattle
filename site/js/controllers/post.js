63dc34a8fe5eadb55de391a3edab81922cbbc320
Lorenzo Affetti <affox90@gmail.com>
2014-05-24 17:45:53 +0200

    [frontend] post editing and deleting

--- site/js/controllers/post.js
+++ site/js/controllers/post.js
@@ -5,10 +5,41 @@ angular.module('post', ['restangular'])
   $scope.post_selected = 0;
 })
 
-.controller('PostComments',
+.controller('Post',
   function($scope, Restangular){
     $scope.comment = {};
+    $scope.editPost = { content: $scope.post.content };
     $scope.showing = false;
+    $scope.editing = false;
+
+    $scope.toggle_editing = function(){
+      $scope.editing = !$scope.editing;
+    }
+
+    $scope.delete_post = function(post_id){
+      Restangular.one('posts', post_id).remove()
+      .then(
+        function(response){
+          for(var i = 0; i < $scope.posts.length; i++){
+            if($scope.posts[i].id == post_id){
+              $scope.posts.splice(i, 1);
+            }
+          }
+        }
+      );
+    };
+    $scope.edit_post = function(post_id){
+      var post =  Restangular.one('posts', post_id);
+      console.log(post);
+      post.content = $scope.editPost.content;
+      post.put().then(
+        function(response){
+          //update what you are seeing
+          $scope.post.content = post.content;
+          $scope.toggle_editing();
+        }
+      );
+    };
 
     $scope.load_comments = function(post_id){
       Restangular.one('posts', post_id).all('comments').getList()
