e1edc5ab9a3922e868b08dbf1616baa553c74cac
Lorenzo Affetti <affox90@gmail.com>
2014-06-04 17:21:32 +0200

    [heroku]

--- site/server.js
+++ site/server.js
@@ -1,7 +1,10 @@
 var connect = require('connect');
 var PORT = 3000;
+if(process.argv[2]){
+	PORT = process.argv[2];
+}
 var INDEX = 'html/index.html';
 
 connect().use(connect.static(__dirname, {index: INDEX})).listen(PORT);
 console.log('Connect server started...');
-console.log('listening on localhost:3000...');
+console.log('listening on localhost:' + PORT + '...');
