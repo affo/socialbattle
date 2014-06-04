e240f3f4d391c8c0fc6a18911940eb01be7138e4
Lorenzo Affetti <affox90@gmail.com>
2014-06-04 16:57:07 +0200

    procfile

--- web.sh
+++ web.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ "$API" == "true" ]; then
+if [ $API == 1 ]; then
 	python manage.py runserver 0.0.0.0:$PORT --noreload
 else
 	node site/server.js
 fi
+
+#BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-nodejs
+#BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python
