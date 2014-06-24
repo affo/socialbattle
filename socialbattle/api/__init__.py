ac9538982572ac410c3db7f25e718f7aac740e2c
Lorenzo Affetti <affox90@gmail.com>
2014-06-24 15:22:30 +0200

    let's deploy on heroku

--- socialbattle/api/__init__.py
+++ socialbattle/api/__init__.py
@@ -1,10 +1,14 @@
 from socialbattle import settings
-from pusher import Pusher
+from pusher import Pusher, pusher_from_url
 from django.core.serializers.json import DjangoJSONEncoder
+import os
 
-pusher = Pusher(
-	app_id=settings.PUSHER_APP_ID,
-	key=settings.PUSHER_APP_KEY,
-	secret=settings.PUSHER_APP_SECRET,
-	encoder=DjangoJSONEncoder
-)
+if os.environ.get('HEROKU'):
+	pusher = pusher_from_url()
+else:
+	pusher = Pusher(
+		app_id=settings.PUSHER_APP_ID,
+		key=settings.PUSHER_APP_KEY,
+		secret=settings.PUSHER_APP_SECRET,
+		encoder=DjangoJSONEncoder
+	)
